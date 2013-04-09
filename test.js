51de4fbd2c6aba5a2569c02fe70b95cc558540db
Robert Sköld <robert@publicclass.se>
2013-04-09 17:44:39 +0200

    Release 0.0.1

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 var now = require(typeof __dirname != 'undefined' ? './' : 'now');
 
-console.log('1..3')
+console.log('1..4')
 assert(!isNaN(now()))
 assert(typeof now() == 'number')
 assert(now() > 0)
+var n = now();
+setTimeout(function(){
+  assert(n!==now())
+},10)
 
 function assert(ok,desc){
   assert.i = (assert.i || 0) + 1;
